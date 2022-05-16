5e68a142017ab9e8e065b35764e0ff460b7a28c3
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2022-05-16 20:38:09 +0530

    rec-intro(day-1)

---
+++ rec-int/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'  
+import Nav from './components/nav'  
+import Mobile from './components/mobile' 
+
+function App() {  
+  return (
+    <div className="App">
+      <Nav/>  
+      <Mobile/> 
+    </div>
+  )
+}
+
+export default App
---
+++ rec-int/src/components/nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav() {
+  return (   
+    <nav>  
+      <h3> LOGOBAKERY </h3>   
+      <div>  
+        <a> Services </a>   
+        <a> Projects </a>   
+        <a> About </a>  
+      </div>  
+      <button> Contact </button>  
+    </nav>
+  );
+}
+
+export default Nav;
