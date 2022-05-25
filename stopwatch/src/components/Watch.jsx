3ee05352497ea3caad9301ef6445828a76c9584d
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2022-05-25 23:47:46 +0530

    stopwatch-day

---
+++ stopwatch/src/components/Watch.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {useState} from "react"; 
+
+const Watch = () => {
+    const [timeid,settimeid] = useState();
+    const [watch, setwatch] = useState(0);  
+
+    const start = ()=>{
+        if(!timeid){
+            let id = setInterval(()=>{
+                setwatch((prev)=>prev +1)
+            },100); 
+            settimeid(id);  
+        }
+    };  
+    const pause =()=>{
+        clearInterval(timeid); 
+        settimeid(null) 
+    }; 
+    const reset =()=>{
+        clearInterval(timeid); 
+        setwatch(0); 
+    }
+
+  return (
+    <div>
+        <h2> Stopwatch </h2> 
+        <h1> {watch} </h1>
+        <button onClick={start}> Start </button>
+        <button onClick={pause}> Pause </button>
+        <button onClick={reset}> Reset </button>
+    </div>
+  )
+}
+
+export default Watch
