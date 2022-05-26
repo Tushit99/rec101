4c9ad959b1da2295ff98d07924a3d6082fae1083
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2022-05-26 22:39:19 +0530

    stopwatch

--- stopwatch/src/App.jsx
+++ stopwatch/src/App.jsx
@@ -1,12 +1,31 @@
-import Watch from './components/Watch'
-import './App.css'
+import Watch from "./components/Watch";
+import "./App.css";
 
-function App() { 
+function App() {
   return (
-    <div className="App">
-     <Watch /> 
+    <div className="main">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfLMoi_9OhNY3E_qJrloadsWu2Q7-TCM8T1Q&usqp=CAU"
+        alt=""
+      />
+      <div className="display">
+        <div>
+          <div className="App">
+            <Watch />
+          </div>
+          <div className="box">
+            <p>
+              A stopwatch is a timepiece designed to measure the amount of time
+              that elapses between its activation and deactivation.
+            </p>
+          </div>
+        </div>
+        <div>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnjV003BSJrSIz5cg826DMr13Tnf4w51qb6g&usqp=CAU" className="imag" alt="" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- stopwatch/src/components/Watch.jsx
+++ stopwatch/src/components/Watch.jsx
@@ -1,39 +1,42 @@
-import React from 'react'
-import {useState} from "react"; 
+import React, { useRef } from "react";
+import { useState } from "react";
 
 const Watch = () => {
-    // const [timeid,settimeid] = useState();
-    // const [watch, setwatch] = useState(0);  
+  // const [timeid,settimeid] = useState();
+  // const [watch, setwatch] = useState(0);
 
-    const timerid = useRef()
+  const timerid = useRef(null);
+  const [watch, setwatch] = useState(0);
 
-
-    const start = ()=>{
-        if(!timeid){
-            let id = setInterval(()=>{
-                setwatch((prev)=>prev +1)
-            },100); 
-            (id);  
-        }
-    };  
-    const pause =()=>{
-        clearInterval(timeid); 
-        settimeid(null) 
-    }; 
-    const reset =()=>{
-        clearInterval(timeid); 
-        setwatch(0); 
+  const start = () => {
+    if (!timerid.current) {
+      let id = setInterval(() => {
+        setwatch((prev) => prev + 1);
+      }, 100);
+      timerid.current = id;
     }
+  };
+  const pause = () => {
+    clearInterval(timerid.current);
+    timerid.current = null;
+  };
+  const reset = () => {
+    clearInterval(timerid.current);
+    setwatch(0);
+    timerid.current = null;
+  };
 
   return (
     <div>
-        <h2> Stopwatch </h2> 
-        <h1> {watch} </h1>
+      <h2> Stop watch </h2>
+      <h1> {watch} </h1>
+      <div className="but">
         <button onClick={start}> Start </button>
         <button onClick={pause}> Pause </button>
         <button onClick={reset}> Reset </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Watch
+export default Watch;
